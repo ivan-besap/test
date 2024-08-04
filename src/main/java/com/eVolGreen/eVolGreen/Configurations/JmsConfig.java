24e36518822e8b302984d77ee83974b1bd7479b7
ivan-besap <ivan@besap.cl>
2024-08-04 00:55:01 -0400

    (Amazon MQ)

--- src/main/java/com/eVolGreen/eVolGreen/Configurations/JmsConfig.java
+++ src/main/java/com/eVolGreen/eVolGreen/Configurations/JmsConfig.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jms.annotation.EnableJms;
+import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
+import org.springframework.jms.config.JmsListenerContainerFactory;
 import org.springframework.jms.connection.CachingConnectionFactory;
 import org.springframework.jms.core.JmsTemplate;
 
@@ -38,5 +40,13 @@ public class JmsConfig {
     @Bean
     public JmsTemplate jmsTemplate() {
         return new JmsTemplate(cachingConnectionFactory());
+    }
+
+    @Bean
+    public JmsListenerContainerFactory<?> jmsListenerContainerFactory() {
+        DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
+        factory.setConnectionFactory(cachingConnectionFactory());
+        factory.setConcurrency("1-1");
+        return factory;
     }
 }
